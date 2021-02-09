c3d9b9be742bd46427245911b9216d74c4b231c2
Yudho Alfantyo <nosvengeance@gmail.com>
2021-02-09 11:53:37 +0700

    Fixed User Profile bug

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -64,11 +64,24 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'photo' => 'profile.png',
             'password' => Hash::make($data['password']),
         ]);
+
+        $user->profile()->create([
+            'user_id' => $user->id,
+            'bio' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            'username' => '@username',
+            'headline' => 'Your Headline',
+            'facebook' => 'Your Facebook',
+            'instagram' => '@instagram',
+            'twitter' => '@twitter',
+            'phone' => 'Your Phone Number'
+        ]);
+
+        return $user;
     }
 }

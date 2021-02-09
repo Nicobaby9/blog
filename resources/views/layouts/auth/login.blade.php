d6b75e535621c8eaf12e96a72b2ceb95717201a6
Yudho Alfantyo <nosvengeance@gmail.com>
2021-02-09 08:05:21 +0700

    Login Page Custom

---
+++ resources/views/layouts/auth/login.blade.php
@@ -0,0 +1,79 @@
+@extends('layouts.auth.app')
+
+@section('content')
+<div class="card card-primary">
+  <div class="card-header"><h4>Login</h4></div>
+
+  <div class="card-body">
+    <form method="POST" action="{{ route('login') }}" class="needs-validation" novalidate="">
+      @csrf
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input id="email" type="email" class="form-control" name="email" tabindex="1" value="{{ old('email') }}" required autofocus>
+        @error('email')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+        @enderror
+        <div class="invalid-feedback">
+          Please fill in your email
+        </div>
+      </div>
+
+      <div class="form-group">
+        <div class="d-block">
+            <label for="password" class="control-label">Password</label>
+          <div class="float-right">
+            <a href="{{ route('password.request') }}" class="text-small">
+              Forgot Password?
+            </a>
+          </div>
+        </div>
+        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" tabindex="2" required autocomplete="current-password" {{ old('remember') ? 'checked' : '' }}>
+        @error('password')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+        @enderror
+        <div class="invalid-feedback">
+          please fill in your password
+        </div>
+      </div>
+
+      <div class="form-group">
+        <div class="custom-control custom-checkbox">
+          <input type="checkbox" name="remember" class="custom-control-input" tabindex="3" id="remember-me">
+          <label class="custom-control-label" for="remember-me">Remember Me</label>
+        </div>
+      </div>
+
+      <div class="form-group">
+        <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
+          Login
+        </button>
+        @if (Route::has('password.request'))
+            <a class="btn btn-link" href="{{ route('password.request') }}">
+                {{ __('Forgot Your Password?') }}
+            </a>
+        @endif
+      </div>
+    </form>
+    <!-- <div class="text-center mt-4 mb-3">
+      <div class="text-job text-muted">Login With Social</div>
+    </div>
+    <div class="row sm-gutters">
+      <div class="col-6">
+        <a class="btn btn-block btn-social btn-facebook">
+          <span class="fab fa-facebook"></span> Facebook
+        </a>
+      </div>
+      <div class="col-6">
+        <a class="btn btn-block btn-social btn-twitter">
+          <span class="fab fa-twitter"></span> Twitter
+        </a>
+      </div>
+    </div> -->
+    Don't have an account? <a href="{{ route('register') }}">Create One</a>
+  </div>
+</div>
+@endsection

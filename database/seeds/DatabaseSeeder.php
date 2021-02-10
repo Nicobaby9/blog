5ab7dcb833a8b69d73ec821d8f5f4727b4b77774
Yudho Alfantyo <nosvengeance@gmail.com>
2021-02-11 06:53:21 +0700

    Database Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call(WebSettingSeeder::class);
+        $this->call(ContactSeeder::class);
+        $this->call(UserSeeder::class);
+        $this->call(CategorySeeder::class);
+        $this->call(TagSeeder::class);
+        $this->call(PostSeeder::class);
+        $this->call(PostTagSeeder::class);
     }
 }

5774e70f86e5f272862a1b6ee3571ece812543c0
Yudho Alfantyo <nosvengeance@gmail.com>
2021-02-16 06:43:37 +0700

    Done

--- resources/views/layouts/backend/partials/sidebar.blade.php
+++ resources/views/layouts/backend/partials/sidebar.blade.php
@@ -1,14 +1,14 @@
 <div class="main-sidebar">
   <aside id="sidebar-wrapper">
     <div class="sidebar-brand">
-      <a href="{{ route('dashboard') }}">{{ $web->web_name }}</a>
+      <a href="{{ route('admin.dashboard') }}">{{ $web->web_name }}</a>
     </div>
     <div class="sidebar-brand sidebar-brand-sm">
       <a href="index.html">St</a>
     </div>
     <ul class="sidebar-menu">
         <li class="menu-header">Dashboard</li>
-        <li class="active"><a class="nav-link" href="{{ route('dashboard') }}"><i class="fas fa-tachometer-alt"></i> <span>Dashboard</span></a></li>
+        <li class="active"><a class="nav-link" href="{{ route('admin.dashboard') }}"><i class="fas fa-tachometer-alt"></i> <span>Dashboard</span></a></li>
         @if(auth()->user()->role == 1)
         <li class="menu-header">Menu</li>
         <li class="nav-item dropdown">
